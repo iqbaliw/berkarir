3d091a8a2d0c6f861e8f75ed42abb880febd14bf
iqbaliw <iqbalwahyudi20@gmail.com>
2017-10-03 03:16:25 +0700

    layout assessment lengkapi

---
+++ application/modules/Assessment/views/lengkapi.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col-md-3">
+        <div class="box box-default collapsed-box box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">Expandable</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              The body of the box
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+</div>
